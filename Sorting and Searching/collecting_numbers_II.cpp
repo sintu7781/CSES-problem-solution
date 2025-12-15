42c2345edecf93f73d9cf975bee364d534de46b2
Sintu Kumar <sintukr7781@gmail.com>
2025-12-15 14:12:24 +0530

    Refactor collecting_numbers_II.cpp for improved logic and performance
    - Adjusted vector sizes to accommodate edge cases.
    - Initialized vector values for better clarity and correctness.
    - Modified loop indices and conditions to enhance readability.
    - Improved the logic for counting valid pairs during swaps.
    - Ensured proper updates to the tracking vectors after each swap.

--- Sorting and Searching/collecting_numbers_II.cpp
+++ Sorting and Searching/collecting_numbers_II.cpp
@@ -4,23 +4,37 @@ using namespace std;
 int main() {
     int n, m;
     cin >> n >> m;
-    vector<int> v(n+1, -1);
-    vector<int> t(n+1);
-    int ans = 1;
-    for (int i = i; i <= n;i++) {
+    vector<int> v(n+2, -1);
+    vector<int> t(n+2);
+    v[0] = 0;
+    v[n + 1] = n + 1;
+    int ans = 0;
+    for (int i = 1; i <= n;i++) {
         cin >> t[i];
-        if(v[t[i]+1] != -1)
+        if(v[t[i]+1] != -1) 
             ans++;
-        v[t[i]] = t[i];
+        
+        v[t[i]] = i;
     }
-    cout << ans << endl;
     while(m--) {
         int a, b;
         cin >> a >> b;
-        if(t[a] < t[b])
-            ans++;
-        else
+        if(a > b)
+            swap(a, b);
+        if(t[a] == t[b]+1)
             ans--;
+        if(t[a] == t[b]-1)
+            ans++;
+        if(v[t[a]+1] > a && v[t[a]+1] < b)
+            ans++;
+        if(v[t[a]-1] > a && v[t[a]-1] < b)
+            ans--;
+        if(v[t[b]-1] > a && v[t[b]-1] < b)
+            ans++;
+        if(v[t[b]+1] > a && v[t[b]+1] < b)
+            ans--;
+        v[t[a]] = b;
+        v[t[b]] = a;
         swap(t[a], t[b]);
         cout << ans << endl;
     }
