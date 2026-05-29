d0d48882db5fe0e31059692fea10e5bbfb39026f
Sintu Kumar <sintukr7781@gmail.com>
2026-05-29 21:45:55 +0530

    Add solution for permutations problem

---
+++ Introductory Problems/permutations.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pii = pair<int,int>;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr)
+
+const ll INFLL = 1e18;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+
+int main() {
+    fastio;
+    int n;
+    cin >> n;
+    if(n > 1 && n < 4) {
+        cout << "NO SOLUTION";
+    } else {
+        for (int i = 2; i <= n;i+=2)
+            cout << i << " ";
+        for (int i = 1; i <= n;i+=2)
+            cout << i << " ";
+    }
+
+    return 0;
+}
